9dd65e7b3c8736c9b29a5472449cb5763df2d466
svuppala2024 <svuppala1@wpi.edu>
2023-08-25 19:53:28 -0400

    sign in page

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -4,8 +4,29 @@ import "../pages/SignIn.css";
 
 function SignIn() {
   return (
-    <div>
-      hello
+    <div className="backgroundColor">
+      <div className="box">
+        <div className="block">
+          <div className="signintext">
+            Sign-In
+          </div>
+          <div className="form">
+            <form>
+              <div className="center padding">
+                <label>Username</label><br></br>
+                <input type="text" id="username" name="username" className="fieldwide"></input><br></br>
+              </div>
+              <div className="center padding">
+                <label>Password</label><br></br>
+                <input type="password" id="password" name="password" className="fieldwide"></input>
+              </div>
+              <div className="center buttonwide padding">
+              <input type="submit" className = "center" value="Login"></input>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

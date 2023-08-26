f6b8c24813edcaee8efeb5f4400cc35819fedbc3
PranavBonthu <143048028+PranavBonthu@users.noreply.github.com>
2023-08-25 20:12:48 -0400

    API stuff

--- src/components/pages/Fit.js
+++ src/components/pages/Fit.js
@@ -1,26 +1,55 @@
-import React from "react";
+//import React from "react";
 import "../../App.css";
 import "../pages/Fit.css";
 
 function Fit() {
-  
-  
-  
-  return (
-    <div>
-      
-     
+  //var zip = prompt("What is your zipcode");
+  // var zip = '01545'
+  // var jsonOut = ''
+  // const api_url = `http://api.weatherapi.com/v1/forecast.json?key=6f403da98865458eb8c195706232508&q=${zip}&days=7`;
+  // fetch(api_url)
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error(`Network response was not ok: ${response.status}`);
+  //     }
+  //     return response.json()
+  //   })
+  //   .then(data => {
+  //     jsonOut = JSON.stringify(data.forecast, null, 2)
+  //     console.log(data.forecast);
+  //   })
+  //   .catch(error=> {
+  //     console.error('Fetch error:', error);
+  //   });
 
-    </div>
-  );
+    return(
+      <text>Hello1</text>
+    );
+  
 }
 
 export default Fit;
 
-function apiData() {
-  var zip = 15;
-  const request = new Request(`http://api.weatherapi.com/v1/forecast.json?key=<6f403da98865458eb8c195706232508>&q=${zip}&days=7)`)
-  const url = request.url;
-  const method = request.method;
-  const credentials = request.credentials;
-}
+// var zip = prompt("What is your zipcode");
+// const api_url = `http://api.weatherapi.com/v1/forecast.json?key=6f403da98865458eb8c195706232508&q=${zip}&days=7)`;
+// fetch(api_url)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(`Network response was not ok: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error=> {
+//     console.error('Fetch error:', error);
+//   });
+
+// function apiData(zip) {
+  // const request = new Request(`http://api.weatherapi.com/v1/forecast.json?key=6f403da98865458eb8c195706232508&q=${zip}&days=7)`)
+  // const url = request.url;
+  // const method = request.method;
+  // const credentials = request.credentials;
+
+//}
